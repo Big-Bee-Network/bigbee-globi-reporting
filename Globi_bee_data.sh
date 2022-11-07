9b25cb58e12efdd137e57dfcaf1e403500717f9f
seltmann <enicospilus@gmail.com>
2022-11-07 12:20:25 -0800

    add new summary stats

--- Globi_bee_data.sh
+++ Globi_bee_data.sh
@@ -7,83 +7,90 @@
 
 
 echo Creating headers
-head -1 output/2021-11-23/indexed_interactions_full.tsv > Andrenidae_data.tsv
-head -1 output/2021-11-23/indexed_interactions_full.tsv > Apidae_data.tsv
-head -1 output/2021-11-23/indexed_interactions_full.tsv > Colletidae_data.tsv
-head -1 output/2021-11-23/indexed_interactions_full.tsv > Halictidae_data.tsv
-head -1 output/2021-11-23/indexed_interactions_full.tsv > Megachilidae_data.tsv
-head -1 output/2021-11-23/indexed_interactions_full.tsv > Melittidae_data.tsv
-head -1 output/2021-11-23/indexed_interactions_full.tsv > Stenotritidae_data.tsv
+head -1 output/2022-11-04/indexed_interactions_full.tsv > Andrenidae_data.tsv
+head -1 output/2022-11-04/indexed_interactions_full.tsv > Apidae_data.tsv
+head -1 output/2022-11-04/indexed_interactions_full.tsv > Colletidae_data.tsv
+head -1 output/2022-11-04/indexed_interactions_full.tsv > Halictidae_data.tsv
+head -1 output/2022-11-04/indexed_interactions_full.tsv > Megachilidae_data.tsv
+head -1 output/2022-11-04/indexed_interactions_full.tsv > Melittidae_data.tsv
+head -1 output/2022-11-04/indexed_interactions_full.tsv > Stenotritidae_data.tsv
 
 #find all Andrenidae and write one file with all data and a second file only with unique records
 
 echo Finding all Andrenidae
-cat output/2021-11-23/indexed_interactions_full.tsv | grep -w "Andrenidae" >> Andrenidae_data.tsv
+cat output/2022-11-04/indexed_interactions_full.tsv | grep -w "Andrenidae" >> Andrenidae_data.tsv
 wc -l Andrenidae_data.tsv
 
 echo Sorting unique records
 sort -r Andrenidae_data.tsv | uniq > Andrenidae_data_unique.tsv
 wc -l Andrenidae_data_unique.tsv
+wc -l Andrenidae_data_unique.tsv >> bee_counts.txt
 #####################################
 #find all Apidae and write one file with all data and a second file only with unique records
 
 echo Finding all Apidae
-cat output/2021-11-23/indexed_interactions_full.tsv | grep -w "Apidae" >> Apidae_data.tsv
+cat output/2022-11-04/indexed_interactions_full.tsv | grep -w "Apidae" >> Apidae_data.tsv
 wc -l Apidae_data.tsv
 
 echo Sorting unique records
 sort -r Apidae_data.tsv | uniq > Apidae_data_unique.tsv
 wc -l Apidae_data_unique.tsv
+wc -l Apidae_data_unique.tsv >> bee_counts.txt
 #####################################
 #find all Colletidae and write one file with all data and a second file only with unique records
 
 echo Finding all Colletidae
-cat output/2021-11-23/indexed_interactions_full.tsv | grep -w "Colletidae" >> Colletidae_data.tsv
+cat output/2022-11-04/indexed_interactions_full.tsv | grep -w "Colletidae" >> Colletidae_data.tsv
 wc -l Colletidae_data.tsv
 
 echo Sorting unique records
 sort -r Colletidae_data.tsv | uniq > Colletidae_data_unique.tsv
 wc -l Colletidae_data_unique.tsv
+wc -l Colletidae_data_unique.tsv >> bee_counts.txt
 #####################################
 #find all Halictidae and write one file with all data and a second file only with unique records
 
 echo Finding all Halictidae
-cat output/2021-11-23/indexed_interactions_full.tsv | grep -w "Halictidae" >> Halictidae_data.tsv
+cat output/2022-11-04/indexed_interactions_full.tsv | grep -w "Halictidae" >> Halictidae_data.tsv
 wc -l Halictidae_data.tsv
 
 echo Sorting unique records
 sort -r Halictidae_data.tsv | uniq > Halictidae_data_unique.tsv
 wc -l Halictidae_data_unique.tsv
+wc -l Halictidae_data_unique.tsv >> bee_counts.txt
 #####################################
 #find all Megachilidae and write one file with all data and a second file only with unique records
 
 echo Finding all Megachilidae
-cat output/2021-11-23/indexed_interactions_full.tsv | grep -w "Megachilidae" >> Megachilidae_data.tsv
+cat output/2022-11-04/indexed_interactions_full.tsv | grep -w "Megachilidae" >> Megachilidae_data.tsv
 wc -l Megachilidae_data.tsv
 
 echo Sorting unique records
 sort -r Megachilidae_data.tsv | uniq > Megachilidae_data_unique.tsv
 wc -l Megachilidae_data_unique.tsv
+wc -l Megachilidae_data_unique.tsv >> bee_counts.txt 
 #####################################
 #find all Melittidae and write one file with all data and a second file only with unique records
 
 echo Finding all Melittidae
-cat output/2021-11-23/indexed_interactions_full.tsv | grep -w "Melittidae" >> Melittidae_data.tsv
+cat output/2022-11-04/indexed_interactions_full.tsv | grep -w "Melittidae" >> Melittidae_data.tsv
 wc -l Melittidae_data.tsv
 
 echo Sorting unique records
 sort -r Melittidae_data.tsv | uniq > Melittidae_data_unique.tsv
 wc -l Melittidae_data_unique.tsv
+wc -l Melittidae_data_unique.tsv >> bee_counts.txt
 #####################################
 #find all Stenotritidae and write one file with all data and a second file only with unique records
 
 echo Finding all Stenotritidae
-cat output/2021-11-23/indexed_interactions_full.tsv | grep -w "Stenotritidae" >> Stenotritidae_data.tsv
+cat output/2022-11-04/indexed_interactions_full.tsv | grep -w "Stenotritidae" >> Stenotritidae_data.tsv
 wc -l Stenotritidae_data.tsv
 
 echo Sorting unique records
 sort -r Stenotritidae_data.tsv | uniq > Stenotritidae_data_unique.tsv
 wc -l Stenotritidae_data_unique.tsv
+wc -l Stenotritidae_data_unique.tsv >> bee_counts.txt
 
 #####################################
 #create one large bee file
@@ -91,6 +98,9 @@ cat *unique.tsv >> all_bee_data.txt
 sort -r all_bee_data.txt | uniq > indexed_interactions_bees.tsv
 
 #####################################
-#get counts for each collection
-cat output/2021-11-23/indexed_interactions_bees.tsv | cut -f6 | sort > out.txt
-cat out.txt | uniqu -c
+#get counts for each collection and prints
+cat indexed_interactions_bees.tsv | cut -f6 | sort > out2.txt
+cat out2.txt | uniq -c >> bee_counts.txt
+rm out2.txt
+cat bee_counts.txt
+
--- README.md
+++ README.md
@@ -13,6 +13,9 @@ For archived reports from Big Bee project reports see
 
 ## Creating a new archived report
 0. Update elton to get the most recent version
+
+[update to new version of generate-report.sh?]
+
 1. Make a list of GloBI/Elton index configurations species interaction datasets. (e.g., see https://raw.githubusercontent.com/globalbioticinteractions/globalbioticinteractions.github.io/master/_data/bigbee.tsv)
 
 Example:
@@ -34,7 +37,7 @@ globalbioticinteractions/unhc-unhc
 
 2. Save the list to a file (e.g., datasets.tsv)
 
-3. run report script: generate-report.sh [datasets.tsv] 
+3. Run report script: generate-report.sh [datasets.tsv] 
 
 e.g., 
 
@@ -43,4 +46,8 @@ $ ./generate-report.sh datasets.tsv
 ...
 ```
 
-4. after generating the report, the results should be available in the ```output/``` folder. 
+4. After generating the report, the results should be available in the ```output/``` folder. 
+
+5. Run Globi_bee_data.sh to generate report of just bee interactions by collection
+
+6. Create a new Zenodo verson. Copy the files over to the new version and update the text. Include the summary statistics for Bee Library in the text and in an associated PDF file.
